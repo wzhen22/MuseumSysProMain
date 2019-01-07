caeadcb9c088e16d05298299e226fcc69f204733
wzhen22 <qwaszx375204@126.com>
2019-01-07 09:21:00 +0800

    Initial Commit

---
+++ MuseumSysPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MuseumSysPro
+//
+//  Created by admin on 2019/1/7.
+//  Copyright © 2019年 cbg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MuseumSysPro/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MuseumSysPro
+//
+//  Created by admin on 2019/1/7.
+//  Copyright © 2019年 cbg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
